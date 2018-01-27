646e2a97b679344ce64baa0d6890fafec746719a
Steven J. Foland <stevenfoland@tsogiants.org>
2018-01-27 11:49:42 -0600

    Created game.py

---
+++ game.py
@@ -0,0 +1,16 @@
+class Player:
+	name = "Player"
+	health = 15
+	inventory = []
+	
+	def take_object(self, item):
+		self.inventory.append(object)
+		
+		
+	def drop_object(self, item):
+		for thing in inventory:
+			if (item == thing):
+				self.inventory.pop(thing)
+				
+
+player = Player()
