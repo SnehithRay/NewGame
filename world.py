544ebfc71cf283c4731403a1a0fa48813e7fb385
Steven J. Foland <stevenfoland@tsogiants.org>
2018-02-23 11:18:36 -0600

    Cleaned up MapTile class.

--- world.py
+++ world.py
@@ -3,55 +3,78 @@ import barriers
 
 class MapTile:
 	description = "Do not create raw MapTiles! Create a subclass instead!"
+	barriers = []
+	enemies = []
+	items = []
 	
 	def __init__(self, x=0, y=0, barriers = [], items = [], enemies = []):
 		self.x = x
 		self.y = y
-		self.contents = {'barriers': barriers, 'enemies': enemies, 'items': items}	# A dict containing all the contents of the room.
+		for barrier in barriers:
+			self.add_barrier(barrier)
+		for item in items:
+			self.add_item(item)
+		for enemy in enemies:
+			self.add_enemy(enemy)
 	
 	def intro_text(self):
 		text = self.description
-		for barrier in self.contents['barriers']:
+		for barrier in self.barriers:
 			if(barrier.verbose):
 				text += " " + barrier.description()
 		#for enemy in self.contents['enemies']:
 		#	text += " " + enemy.description()
-		for item in self.contents['items']:
+		for item in self.items:
 			text += " " + item.room_text()
 		return text
 		
 	def handle_input(self, verb, noun1, noun2, inventory):
 		if(not noun2):
 			if(verb == 'check'):
-				for item in self.contents['items']:
+				for item in self.items:
 					if(item.name.lower() == noun1):
 						return [True, item.check_text(), inventory]
 			elif(verb == 'take'):
-				for index in range(len(self.contents['items'])):
-					if(self.contents['items'][index].name.lower() == noun1):
-						pickup_text = "You picked up the %s." % self.contents['items'][index].name
-						inventory.append(self.contents['items'][index])
-						self.contents['items'].pop(index)
+				for index in range(len(self.items)):
+					if(self.items[index].name.lower() == noun1):
+						pickup_text = "You picked up the %s." % self.items[index].name
+						inventory.append(self.items[index])
+						self.items.pop(index)
 						return [True, pickup_text, inventory]
 			elif(verb == 'drop'):
 				for index in range(len(inventory)):
 					if(inventory[index].name.lower() == noun1):
 						inventory[index].is_dropped = True
 						drop_text = "You dropped the %s." % inventory[index].name
-						if(len(self.contents['items']) > 0):
-							self.contents['items'].append(inventory[index])
-						else:
-							self.contents['items'] = [inventory[index]]
+						self.add_item(inventory[index])
 						inventory.pop(index)
 						return [True, drop_text, inventory]
 
-		for key in self.contents.keys():
-			for item in self.contents[key]:
+		for list in [self.barriers, self.items, self.enemies]:
+			for item in list:
 				[status, description, inventory] = item.handle_input(verb, noun1, noun2, inventory)
 				if(status):
 					return [status, description, inventory]
 			
 		return [False, "", inventory]
+		
+	def add_barrier(self, barrier):
+		if(len(self.barriers) == 0):
+			self.barriers = [barrier]		# Initialize the list if it is empty.
+		else:
+			self.barriers.append(barrier)	# Add to the list if it is not empty.
+			
+	def add_item(self, item):
+		if(len(self.items) == 0):
+			self.items = [item]		# Initialize the list if it is empty.
+		else:
+			self.items.append(item)	# Add to the list if it is not empty.
+			
+	def add_enemy(self, enemy):
+		if(len(self.enemies) == 0):
+			self.enemies = [enemy]		# Initialize the list if it is empty.
+		else:
+			self.enemies.append(enemy)	# Add to the list if it is not empty.
 
 
 class StartTile(MapTile):
@@ -60,7 +83,7 @@ class StartTile(MapTile):
 		"""
 
 class Corridor(MapTile):
-	description = """You find yourself in a poorly lit corridor."""
+	description = """You find yourself in a poorly lit corridor."""			
 		
 class StoreRoom(MapTile):
 	description = """You seem to have entered an underground storeroom!"""
@@ -116,7 +139,7 @@ class World:									# I choose to define the world as a class. This makes it mo
 			return None
 			
 	def check_north(self, x, y):
-		for barrier in self.map[y][x].contents['barriers']:
+		for barrier in self.map[y][x].barriers:
 			if(barrier.direction == 'north' and not barrier.passable):
 				return [False, barrier.description()]				
 				
@@ -134,7 +157,7 @@ class World:									# I choose to define the world as a class. This makes it mo
 			return [False, "There doesn't seem to be a path to the north."]
 			
 	def check_south(self, x, y):
-		for barrier in self.map[y][x].contents['barriers']:
+		for barrier in self.map[y][x].barriers:
 			if(barrier.direction == 'south' and not barrier.passable):
 				return [False, barrier.description()]	
 				
@@ -152,7 +175,7 @@ class World:									# I choose to define the world as a class. This makes it mo
 			return [False, "There doesn't seem to be a path to the south."]
 
 	def check_west(self, x, y):
-		for barrier in self.map[y][x].contents['barriers']:
+		for barrier in self.map[y][x].barriers:
 			if(barrier.direction == 'west' and not barrier.passable):
 				return [False, barrier.description()]	
 	
@@ -170,7 +193,7 @@ class World:									# I choose to define the world as a class. This makes it mo
 			return [False, "There doesn't seem to be a path to the west."]
 			
 	def check_east(self, x, y):
-		for barrier in self.map[y][x].contents['barriers']:
+		for barrier in self.map[y][x].barriers:
 			if(barrier.direction == 'east' and not barrier.passable):
 				return [False, barrier.description()]	
 				
